bb07dd3bb65d30284867cdcca02a889b2d191d01
Jennifer Kong <jkong1@andrew.cmu.edu>
2019-12-02 01:41:21 -0500

    animation added

--- assgn8/startbootstrap-freelancer-gh-pages/sketch.js
+++ assgn8/startbootstrap-freelancer-gh-pages/sketch.js
@@ -1,7 +1,30 @@
+let x = 1;
+let y = 1;
+let easing = 0.05;
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+    background(255,255,255,25); //white
+
+    //background(0,0,0,25); //black
+    //noStroke();
+    //https://p5js.org/examples/input-easing.html
+    let targetX = mouseX;
+    let dx = targetX - x;
+    x += dx * easing;
+
+    let targetY = mouseY;
+    let dy = targetY - y;
+    y += dy * easing;
+    // circle(x, y, 20);
+    // line(mouseX, mouseY, mouseX, windowHeight);
+    textSize(25);
+    text("♥", x, y, 20);
+    //https://cassietarakajian.com/current-projects/
+    var xColor= map(mouseX, 0, 400, 0,255);
+    var yColor = map(mouseY, 0, 400, 0,255);
+    fill(0,xColor-150, yColor);
   // put drawing code here
 }

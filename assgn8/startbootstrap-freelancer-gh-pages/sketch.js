b99e3d1f601856e807169bcaa0bc7a769dec6cbd
Jennifer Kong <jkong1@andrew.cmu.edu>
2019-12-04 00:09:13 -0500

    added typed.js plugin

--- assgn8/startbootstrap-freelancer-gh-pages/sketch.js
+++ assgn8/startbootstrap-freelancer-gh-pages/sketch.js
@@ -3,10 +3,11 @@ let y = 1;
 let easing = 0.05;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  //background(255,255,255,25);
 }
 
 function draw() {
-    background(255,255,255,25); //white
+    background(255,255,255,100); //white
 
     //background(0,0,0,25); //black
     //noStroke();
@@ -21,11 +22,13 @@ function draw() {
     // circle(x, y, 20);
     // line(mouseX, mouseY, mouseX, windowHeight);
     textSize(25);
-    text("♥", x, y, 20);
-    //https://cassietarakajian.com/current-projects/
-    var xColor= map(mouseX, 0, 400, 0,255);
-    var yColor = map(mouseY, 0, 400, 0,255);
-    fill(0,xColor-150, yColor);
+    //text("✏️", x, y, 20);
+    text("✐", mouseX-17, mouseY, 20);
 
-  // put drawing code here
+    //https://cassietarakajian.com/current-projects/
+    var xColor= map(mouseX, 0, mouseX, 0,225);
+    var yColor = map(mouseY, 0, mouseY, 0,225);
+    fill("gray");
+    //fill(0, xColor, yColor);
+
 }

80b62c5fb2ed6125048aeb6296ee9a6a4f94ec34
Jennifer Kong <jkong1@andrew.cmu.edu>
2019-12-04 20:31:11 -0500

    style fixes

--- assgn8/startbootstrap-freelancer-gh-pages/sketch.js
+++ assgn8/startbootstrap-freelancer-gh-pages/sketch.js
@@ -1,17 +1,15 @@
+//https://p5js.org/examples/input-easing.html cursor easing from here
 let x = 1;
 let y = 1;
-let easing = 0.05;
+let easing = 0.2;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  //background(255,255,255,25);
+  createCanvas(windowWidth, windowHeight);;
 }
 
 function draw() {
     background(255,255,255,100); //white
 
-    //background(0,0,0,25); //black
-    //noStroke();
-    //https://p5js.org/examples/input-easing.html
     let targetX = mouseX;
     let dx = targetX - x;
     x += dx * easing;
@@ -19,16 +17,9 @@ function draw() {
     let targetY = mouseY;
     let dy = targetY - y;
     y += dy * easing;
-    // circle(x, y, 20);
-    // line(mouseX, mouseY, mouseX, windowHeight);
-    textSize(25);
-    //text("✏️", x, y, 20);
-    text("✐", mouseX-17, mouseY, 20);
 
-    //https://cassietarakajian.com/current-projects/
-    var xColor= map(mouseX, 0, mouseX, 0,225);
-    var yColor = map(mouseY, 0, mouseY, 0,225);
-    fill("gray");
-    //fill(0, xColor, yColor);
+    textSize(25);
+    text("✐", x-17, y, 20);
+    fill("grey");
 
 }
